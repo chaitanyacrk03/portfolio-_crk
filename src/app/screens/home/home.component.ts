2df368e6f38f25acdcde7b199d996ceadd00c014
Chaitanya Thakur <chaitanyacrk03@gmail.com>
2023-08-21 01:25:22 +0530

    circle rotate text

--- src/app/screens/home/home.component.ts
+++ src/app/screens/home/home.component.ts
@@ -3,12 +3,21 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
   constructor() { }
-
+data=[]
+dataFrag=[]
   ngOnInit(): void {
+   this.data= ["Angular.", "SpringBoot.", "Python.", "Java.", "JavaScript."]
+   this.data.forEach((ele)=>{
+    var letter=ele.split("")
+    for(let i of letter){
+      this.dataFrag.push(i)
+    }
+   })
+   let val=this.dataFrag;
+   
   }
-
 }
